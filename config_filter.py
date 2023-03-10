2aab5564f12c884d41ed7487d5b6c5648d819fde
master <397498896@qq.com>
2023-03-10 18:11:15 +0800

    V0.1版本  config_filter

--- config_filter.py
+++ config_filter.py
@@ -1,13 +1,16 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QTableView, QPushButton, QVBoxLayout,  QLineEdit, QHBoxLayout, QWidget,  QMainWindow, \
-    QTableWidget, QHeaderView, QTableWidgetItem, QAction, QFileDialog, QDialog, QDialogButtonBox
-from PyQt5.QtGui import QStandardItemModel, QStandardItem, QColor, QIcon
-from PyQt5.QtCore import QSettings, Qt, QSortFilterProxyModel, QRegExp
+from PyQt5.QtWidgets import QApplication, QLabel, QTableView, QVBoxLayout, QLineEdit, QHBoxLayout, QWidget, \
+    QMainWindow, QAction, QDialog, QDialogButtonBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QSettings, Qt, QSortFilterProxyModel
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from logzero import logger
+import xlrd
+
 
 # 要重写QSortFilterProxyModel类
 class SortFilterProxyModel(QSortFilterProxyModel):
+
     def __init__(self, *args, **kwargs):
         QSortFilterProxyModel.__init__(self, *args, **kwargs)
         self.filter_column = 0  # 设置默认过滤列为第一列
@@ -23,11 +26,12 @@ class SortFilterProxyModel(QSortFilterProxyModel):
         else:
             return False
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.search_column = 0
-        self.settings = QSettings('MyCompany', 'MyApp')
+        self.settings = QSettings('Youkia', 'Config_Filter')
         # Create main widget
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
@@ -51,42 +55,93 @@ class MainWindow(QMainWindow):
         search_layout.addWidget(self.search_label)
         search_layout.addWidget(self.search_edit)
 
-
-
         main_layout = QVBoxLayout(central_widget)
         main_layout.addLayout(search_layout)
         main_layout.addWidget(self.table_view)
 
-        # Create layout  正确的
-        # layout = QVBoxLayout(central_widget)
-        # layout.addWidget(self.search_edit)
-        # layout.addWidget(self.table_view)
-        # self.init_table()
-
         self.statusBar()
 
-        # 创建打开文件的动作
-        openFile = QAction('打开', self)
-        openFile.setShortcut('Ctrl+O')
+        # # 创建打开文件的动作
+        # openFile = QAction('打开', self)
+        # openFile.setShortcut('Ctrl+O')
 
         # 创建设置配置文件的动作
-        setFilePath = QAction('设置文件路径', self)
+        setFilePath = QAction('设置读取文件路径', self)
+        setFilePath.setShortcut('Ctrl+O')
         setFilePath.triggered.connect(self.set_file_path)
 
         # 创建菜单栏
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('文件')
-        fileMenu.addAction(openFile)
+        # fileMenu.addAction(openFile)
         fileMenu.addAction(setFilePath)
 
-        self.load_path_data()
-        try:
+        self.init_local_data()
 
+        # Set window properties
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowIcon(QIcon('demo2.ico'))
+        self.setWindowTitle("表格筛选")
+
+    def init_table(self, file_path):
+        # 设置表格视图属性
+        self.table_view.setSortingEnabled(True)
+        self.table_view.setEditTriggers(QTableView.NoEditTriggers)
+        self.table_view.setSelectionBehavior(QTableView.SelectRows)
+        self.table_view.setSelectionMode(QTableView.SingleSelection)
+        if file_path.endswith('.txt'):
+            with open(r'' + file_path, 'r', encoding='UTF-8') as f:
+                try:
+                    content = f.read()
+                except Exception as e:
+                    self.statusBar().showMessage("文件读取失败：" + str(e))
+                _list = content.split('\n')
+                title_row = _list[0].split("\t")
+                # title_row = [i for i in title_row if i.strip() != ""]
+                self.model = QStandardItemModel(len(_list), len(title_row))
+                self.model.setHorizontalHeaderLabels(title_row)
+                self.table_view.setModel(self.model)
+
+                # 在表格控件中显示列内容
+                try:
+                    for m, val in enumerate(_list[1:]):
+                        if val:
+                            # val 为每行的数据
+                            cell_val_list = val.split("\t")
+                            for i, cell in enumerate(cell_val_list):
+                                item = QStandardItem(str(cell))
+                                self.model.setItem(m, i, item)
+                        else:
+                            continue
+                except Exception as e:
+                    self.statusBar().showMessage("初始化表格数据失败：" + str(e))
+        elif file_path.endswith('.xlsx') or file_path.endswith('.xls') or file_path.endswith('.xlsm'):
+            workbook = xlrd.open_workbook(file_path)
+            work_sheet = workbook.sheet_by_index(0)
+            nrows = work_sheet.nrows
+            ncols = work_sheet.ncols
+            title_row = work_sheet.row_values(0)
+            self.model = QStandardItemModel(nrows, ncols)
+            self.model.setHorizontalHeaderLabels(title_row)
+            self.table_view.setModel(self.model)
+
+            # 在表格控件中显示列内容
+            try:
+                for n in range(1, nrows):
+                    cell_val_list = work_sheet.row_values(n)
+                    for i, cell in enumerate(cell_val_list):
+                        c_type = work_sheet.cell(n, i).ctype
+                        if c_type == 2 and cell % 1 == 0.0:  # 处理整数变为浮点数的情况， ctype为2且为浮点
+                            cell = int(cell)
+                        item = QStandardItem(str(cell))
+                        self.model.setItem(n, i, item)
+            except Exception as e:
+                self.statusBar().showMessage("初始化表格数据失败：" + str(e))
+
+        try:
             # 创建过滤器模型
             self.filter_model = SortFilterProxyModel()
             self.filter_model.setSourceModel(self.model)
-
-
 
             # 设置表格视图模型
             self.table_view.setModel(self.filter_model)
@@ -99,68 +154,23 @@ class MainWindow(QMainWindow):
         except Exception as e:
             self.statusBar().showMessage("设置过滤模型失败：" + str(e))
 
-        # Set window properties
-        self.setGeometry(100, 100, 800, 600)
-        self.setWindowTitle("表格筛选")
-
-
-    def init_table(self, file_path):
-
-        # 设置表格视图属性
-        self.table_view.setSortingEnabled(True)
-        self.table_view.setEditTriggers(QTableView.NoEditTriggers)
-        self.table_view.setSelectionBehavior(QTableView.SelectRows)
-        self.table_view.setSelectionMode(QTableView.SingleSelection)
-
-        try:
-            with open(r''+file_path, 'r', encoding='UTF-8') as f:
-                try:
-                    content = f.read()
-                except Exception as e:
-                    logger.info(str(e))
-                    self.statusBar().showMessage("文件读取失败：" + str(e))
-                _list = content.split('\n')
-                title_row = _list[0].split("\t")
-                logger.info(title_row)
-                # title_row = [i for i in title_row if i.strip() != ""]
-                self.model = QStandardItemModel(len(title_row), len(_list))
-                self.model.setHorizontalHeaderLabels(title_row)
-                self.table_view.setModel(self.model)
-
-                # 在表格控件中显示列内容
-                for m, val in enumerate(_list[1:]):
-                    if val:
-                        # val 为每行的数据
-                        cell_val_list = val.split("\t")
-                        # logger.info(cell_val_list)
-                        for i, cell in enumerate(cell_val_list):
-                            item = QStandardItem(str(cell))
-                            self.model.setItem(m, i, item)
-                    else:
-                        continue
-            self.init_search_col()
-        except Exception as e:
-            self.statusBar().showMessage("初始化表格数据失败：" + str(e))
-
-
     # 获取保存好的搜索title数据
     def init_search_col(self):
         title_text = self.settings.value('title_text')
-        # logger.info(title_text)
-        # if _data:
-        #     self.title_text = _data
         if title_text:
-            for column in range(self.model.columnCount()):
-                if self.model.headerData(column, Qt.Horizontal) == title_text:
-                    # 找到了，column 就是对应的列索引
-                    self.search_column = column
-                    self.filter_model.setFilterColumn(self.search_column)
-                    self.statusBar().showMessage("找到对应列数据，请输入搜索内容")
-                    break
-            else:
-                # 没有找到
-                self.statusBar().showMessage("未找到对应列，请检查搜索列数据是否正确")
-
+            try:
+                for column in range(self.model.columnCount()):
+                    if self.model.headerData(column, Qt.Horizontal) == title_text:
+                        # 找到了，column 就是对应的列索引
+                        self.search_column = column
+                        self.filter_model.setFilterColumn(self.search_column)
+                        self.statusBar().showMessage("找到对应列数据，请输入搜索内容")
+                        break
+                else:
+                    # 没有找到
+                    self.statusBar().showMessage("未找到对应列，请检查搜索列数据是否正确")
+            except Exception as e:
+                self.statusBar().showMessage("数据异常，请设置文件路径")
 
     def set_search_col(self):
         self.settings.setValue('title_text', self.search_col_edit.text())
@@ -192,11 +202,15 @@ class MainWindow(QMainWindow):
             self.settings.setValue('item_path', data)
             self.init_table(self.settings.value('item_path'))
 
-    def load_path_data(self):
+    def init_local_data(self):
         item_path = self.settings.value('item_path')
-        logger.info(item_path)
         if item_path:
             self.init_table(item_path)
+            try:
+                self.model
+            except Exception as e:
+                self.statusBar().showMessage("表格读取失败： " + str(e))
+            self.init_search_col()
 
 
 if __name__ == "__main__":
